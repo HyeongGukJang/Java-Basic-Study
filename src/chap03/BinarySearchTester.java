115c7029162b66b0c05e8e5e1f0f4bacba112158
= <gudrnr2004@naver.com>
2021-02-20 21:34:00 +0900

    .

---
+++ src/chap03/BinarySearchTester.java
@@ -0,0 +1,36 @@
+package chap03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchTester {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("요솟수 입력 : ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+
+		System.out.println("오름차순으로 입력하세요...");
+
+		System.out.println("x[0] : ");
+		x[0] = sc.nextInt();
+
+		for (int i = 1; i < num; i++) {
+			do {
+				System.out.println("x[" + i + "]");
+				x[i] = sc.nextInt();
+			} while (x[i] < x[i - 1]);
+		}
+		System.out.println("검색할 값 : ");
+		int ky = sc.nextInt();
+		int idx = Arrays.binarySearch(x, ky);
+
+		if (idx < 0) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+		}
+	}
+
+}
